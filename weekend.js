296c1414121bcb7fa3c0c289ff8e8e4fe0773e32
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-08-17 11:50:58 -0500

    added notes comment to notes

--- weekend.js
+++ weekend.js
@@ -1,51 +1,54 @@
-$(document).ready(function(){});
-
-//form submit
-
-var $form = $('#employeeForm');
-//make for each input being submitted
-var $fName = $('#fName');
+////////NOTES//////////
 
 
-$form.on('submit' function(e){
-	e.preventDefault();
-	var employee = new employee(
-		//enter in .val()'s here
+// $(document).ready(function(){});
 
-		);
-	appendemployee();
-});
+// //form submit
 
-//constructor for employee     //make on for each input
-var employee = function(firstName, LastName, etc){
-
-this.firstName = firstName;
-//etc
-}
-
-//object to store employees
-function appendEmployee(emp){
-	var $li = $('<li>');
-	$li.text(emp.firstName + '' + emp.lastName + '' + etc);
-
-	var $button = $('<button>');
-	$button.text('delete');
-	$button.attr('class', 'js-delete');
-	$button.attr('id', 'emp.employeeNum');
-
-	li.append($button);
-
-	$employees.append($li);
-}
-//function to delete employees
-$employees.on('click', '.js-delete', function(e){
-	e.preventDefault();
-	$(this).parent().remove();
-});
-
-//colors per rating
-$li.attr('class', 'score'+emp.score)
+// var $form = $('#employeeForm');
+// //make for each input being submitted
+// var $fName = $('#fName');
 
 
+// $form.on('submit' function(e){
+// 	e.preventDefault();
+// 	var employee = new employee(
+// 		//enter in .val()'s here
 
-});
+// 		);
+// 	appendemployee();
+// });
+
+// //constructor for employee     //make on for each input
+// var employee = function(firstName, LastName, etc){
+
+// this.firstName = firstName;
+// //etc
+// }
+
+// //object to store employees
+// function appendEmployee(emp){
+// 	var $li = $('<li>');
+// 	$li.text(emp.firstName + '' + emp.lastName + '' + etc);
+
+// 	var $button = $('<button>');
+// 	$button.text('delete');
+// 	$button.attr('class', 'js-delete');
+// 	$button.attr('id', 'emp.employeeNum');
+
+// 	li.append($button);
+
+// 	$employees.append($li);
+// }
+// //function to delete employees
+// $employees.on('click', '.js-delete', function(e){
+// 	e.preventDefault();
+// 	$(this).parent().remove();
+// });
+
+// //colors per rating
+// $li.attr('class', 'score'+emp.score)
+
+
+
+// });
