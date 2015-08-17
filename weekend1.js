2b01fb5ad72a00ee2e29954e83083aa6db3d9046
Matthew Brown <MatthewBrown@Matthews-MacBook-Air.local>
2015-08-16 20:38:26 -0500

    saving changes

--- weekend1.js
+++ weekend1.js
@@ -1,21 +1,21 @@
+$(document).ready(function(){
+var newEmployee = [];
 
-var array = ["Matt", "Brown", "1234", "Team Lead", 4, "$75000"]
 
-// var fName = array[0];
-// var lName = array[1];
-// var employeeNum = array[2];
-// var title = array[3];
-// var lRevScore = array[4];
-// var salary = array[5];
+$('#employeeForm').submit(function(e){
+	var $inputs = $('#employeeForm :input');
+	 e.preventDefault();
+	var employee = {};
+	 newEmployee.push(employee);
+	console.log(employee);
+	$inputs.each(function(){
+		 employee[this.name] = $(this).val();
+		 console.log(employee);
+		 $('form').trigger('reset')
+		
+		 console.log(newEmployee);
+    });
 
-var newEmployee = new employee(array);
+	});
 
-function employee(array) {
-	this.firstName = array[0];
-	this.lastName = array[1];
-	this.employeeNumber = array[2];
-	this.title = array[3];
-	this.lastReviewScore = array[4];
-	this.salary = array[5];
-}
-console.log(newEmployee);
+});
